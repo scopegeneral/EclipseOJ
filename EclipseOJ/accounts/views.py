929728f653041bdfec112d11a9ef0595e5bfeb36
Vighnesh Reddy Konda <vighneshreddy98@gmail.com>
2017-09-30 22:48:41 +0530

    Basic login/signup UI
    Issue: While a user is logged in another user can also be logged in

--- EclipseOJ/accounts/views.py
+++ EclipseOJ/accounts/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render_to_response
 from .forms import SignupForm
 from django.template.context_processors import csrf
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import views as auth_views
+from django.shortcuts import render
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
             return HttpResponseRedirect('/')
     args = {}
     args.update(csrf(request))
