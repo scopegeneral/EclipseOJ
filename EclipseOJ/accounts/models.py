967353a7ceee76f471dc33b34a1227411196ef4e
Vamsi Krishna <vamsi3@outlook.com>
2017-10-01 17:49:59 +0530

    Corrected Signup
    Signup now works well

--- EclipseOJ/accounts/models.py
+++ EclipseOJ/accounts/models.py
@@ -13,14 +13,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-post_save.connect(create_user_profile, sender=User)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-post_save.connect(save_user_profile, sender=User)
--- EclipseOJ/accounts/views.py
+++ EclipseOJ/accounts/views.py
@@ -4,21 +4,18 @@ from django.template.context_processors import csrf
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 
 def signup(request):
     if request.method == 'POST':
-        user_form = accounts_forms.UserForm(request.POST, instance=request.user)
-        profile_form = accounts_forms.ProfileForm(request.POST, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
+        user_form = accounts_forms.UserForm(request.POST)
+        if user_form.is_valid():
             new_user = user_form.save()
-            profile_form.save()
-            messages.success(request, _('Your profile was successfully updated!'))
+            #messages.success(request, _('Your profile was successfully updated!'))
             login(request, new_user)
-            return HttpResponseRedirect(reverse('homepage'))
+            return redirect(reverse('homepage'))
     args = {}
     args.update(csrf(request))
     args['user_form'] = accounts_forms.UserForm()
-    args['profile_form'] = accounts_forms.ProfileForm()
     return render_to_response('accounts/signup.html', args)
