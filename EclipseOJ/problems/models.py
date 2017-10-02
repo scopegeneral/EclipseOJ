a07183f0041204498b9c3fc141c8c21a04c09309
Vighnesh Reddy Konda <vighneshreddy98@gmail.com>
2017-10-02 12:08:17 +0530

    Test cases are uploaded by files
    Stored as /testcases/testcases_problemID/filename

--- EclipseOJ/problems/models.py
+++ EclipseOJ/problems/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from contest.models import Contest
 # Create your models here.
 class Problems(models.Model):
+    class Meta:
+        verbose_name_plural = "Problems"
     contest = models.ForeignKey(Contest,on_delete=models.CASCADE)
     #models.CASCADE deletes the Problems of contest if that particular contest is deleted
     contest_alpha = models.CharField(max_length=1)
@@ -11,9 +13,11 @@ class Problems(models.Model):
     def __str__(self):
         return self.problem_name #+ '\n' + self.problem_body
 
+def testcases_directory_path(instance, filename):
+    return 'testcases/testcases_{0}/{1}'.format(instance.problem.problemID, filename)
 
 class TestCases(models.Model):
+    class Meta:
+        verbose_name_plural = "TestCases"
     problem = models.ForeignKey(Problems,on_delete=models.CASCADE)
-    testcase_body = models.TextField()
-    def __str__(self):
-        return self.testcase_body
+    testcase_body = models.FileField(upload_to=testcases_directory_path)
