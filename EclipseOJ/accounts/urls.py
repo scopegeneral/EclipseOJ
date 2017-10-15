b7454ac3953f3cb6ba2f1b23d75c44925ddb5bf3
Naman Jain <naman1205jain@gmail.com>
2017-10-15 16:10:23 +0530

    login redirecting working

--- EclipseOJ/accounts/urls.py
+++ EclipseOJ/accounts/urls.py
@@ -4,8 +4,8 @@ from . import views as accounts_views
 
 
 urlpatterns = [
-    url(r'^login/', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=False), name='login'),
-    url(r'^login/?next=(?P<next>[0-9a-zA-Z_/]+)', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_field_name=next), name='login_direct'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    #url(r'^login/?next=(?P<next>[0-9a-zA-Z_/]+)', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_field_name=next), name='login_direct'),
     url(r'^logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     url(r'^signup/', accounts_views.signup, name='signup'),
 ]
