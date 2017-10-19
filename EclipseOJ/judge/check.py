91a15b1a7ad6bdaf8a522a9ef2adc4d3efa92e8f
Vighnesh Reddy Konda <vighneshreddy98@gmail.com>
2017-10-19 09:53:39 +0530

    Added timeout, supports C with gcc compiler
    at present the languages are coded using numbers;
    can be changed to strings

--- EclipseOJ/judge/check.py
+++ EclipseOJ/judge/check.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-def bashfunc(filename,testcase,number,lang):
+def bashfunc(filename,testcase,number,lang,timeout):
     if lang == 0:
         bashCommand = "g++ -std=c++14 {0}".format(filename)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -13,10 +13,13 @@ def bashfunc(filename,testcase,number,lang):
             for i in range(number):
                 inpi = inputs + str(i+1)
                 outi = outputs + str(i+1)
-                command1 = "./a.out < {0} > tempout.txt".format(inpi)
+                command1 = "./a.out < {0} > tempout.txt & PID=$!; sleep {1}; kill $PID".format(inpi,timeout)
                 command2 = "diff tempout.txt {0} > diff.txt".format(outi)
                 command3 = "rm a.out diff.txt tempout.txt"
-                os.system(command1)
+                result1 = subprocess.call(command1, shell = True)
+                if not result1:
+                    os.system(command3)
+                    return "TLE"
                 os.system(command2)
                 if os.stat("diff.txt").st_size == 0:
                     correct += 1
@@ -32,10 +35,13 @@ def bashfunc(filename,testcase,number,lang):
         for i in range(number):
             inpi = inputs + str(i+1)
             outi = outputs + str(i+1)
-            command1 = "python3 {0} < {1} > tempout.txt".format(filename,inpi)
+            command1 = "python3 {0} < {1} > tempout.txt & PID=$!; sleep {2}; kill $PID".format(filename,inpi,timeout)
             command2 = "diff tempout.txt {0} > diff.txt".format(outi)
             command3 = "rm tempout.txt diff.txt"
-            os.system(command1)
+            result1 = subprocess.call(command1, shell = True)
+            if not result1:
+                os.system(command3)
+                return "TLE"
             os.system(command2)
             if os.stat("diff.txt").st_size == 0:
                 correct += 1
@@ -51,10 +57,13 @@ def bashfunc(filename,testcase,number,lang):
         for i in range(number):
             inpi = inputs + str(i+1)
             outi = outputs + str(i+1)
-            command1 = "python {0} < {1} > tempout.txt".format(filename,inpi)
+            command1 = "python {0} < {1} > tempout.txt & PID=$!; sleep {2}; kill $PID".format(filename,inpi,timeout)
             command2 = "diff tempout.txt {0} > diff.txt".format(outi)
             command3 = "rm tempout.txt diff.txt"
-            os.system(command1)
+            result1 = subprocess.call(command1, shell = True)
+            if not result1:
+                os.system(command3)
+                return "TLE"
             os.system(command2)
             if os.stat("diff.txt").st_size == 0:
                 correct += 1
@@ -76,10 +85,13 @@ def bashfunc(filename,testcase,number,lang):
             for i in range(number):
                 inpi = inputs + str(i+1)
                 outi = outputs + str(i+1)
-                command1 = "java {0} < {1} > tempout.txt".format(filei,inpi)
+                command1 = "java {0} < {1} > tempout.txt & PID=$!; sleep {2}; kill $PID".format(filei,inpi,timeout)
                 command2 = "diff tempout.txt {0} > diff.txt".format(outi)
                 command3 = "rm *.class tempout.txt diff.txt"
-                os.system(command1)
+                result1 = subprocess.call(command1, shell = True)
+                if not result1:
+                    os.system(command3)
+                    return "TLE"
                 os.system(command2)
                 if os.stat("diff.txt").st_size == 0:
                     correct += 1
@@ -88,3 +100,32 @@ def bashfunc(filename,testcase,number,lang):
                     return "WA"
             os.system(command3)
             return "AC"
+    elif lang == 4:
+        bashCommand = "gcc {0}".format(filename)
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        if process.communicate()[1]:
+            return "CE"
+        else:
+            inputs = testcase + "input_"
+            outputs = testcase + "output_"
+            correct = 0
+            for i in range(number):
+                inpi = inputs + str(i+1)
+                outi = outputs + str(i+1)
+                command1 = "./a.out < {0} > tempout.txt & PID=$!; sleep {1}; kill $PID".format(inpi,timeout)
+                command2 = "diff tempout.txt {0} > diff.txt".format(outi)
+                command3 = "rm a.out diff.txt tempout.txt"
+                result1 = subprocess.call(command1, shell = True)
+                if not result1:
+                    os.system(command3)
+                    return "TLE"
+                os.system(command2)
+                if os.stat("diff.txt").st_size == 0:
+                    correct += 1
+                else:
+                    os.system(command3)
+                    return "WA"
+            os.system(command3)
+            return "AC"
+    else:
+        return "no language found"
