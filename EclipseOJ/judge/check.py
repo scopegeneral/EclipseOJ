951417f3767c56f0dcc361e19c05b181033c2651
Vighnesh Reddy Konda <vighneshreddy98@gmail.com>
2017-10-19 09:18:51 +0530

    updated bashfunc such that it takes lang as a parameter
    0 - c++
    1 - py3
    2 - py2
    3 - java

--- EclipseOJ/judge/check.py
+++ EclipseOJ/judge/check.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
-def bashfunc(filename,testcase,number):
-    if filename.endswith('.cpp'):
+def bashfunc(filename,testcase,number,lang):
+    if lang == 0:
         bashCommand = "g++ -std=c++14 {0}".format(filename)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         if process.communicate()[1]:
@@ -15,14 +15,17 @@ def bashfunc(filename,testcase,number):
                 outi = outputs + str(i+1)
                 command1 = "./a.out < {0} > tempout.txt".format(inpi)
                 command2 = "diff tempout.txt {0} > diff.txt".format(outi)
+                command3 = "rm a.out diff.txt tempout.txt"
                 os.system(command1)
                 os.system(command2)
                 if os.stat("diff.txt").st_size == 0:
                     correct += 1
                 else:
+                    os.system(command3)
                     return "WA"
+            os.system(command3)
             return "AC"
-    if filename.endswith('.py'):
+    elif lang == 1:
         inputs = testcase + "input_"
         outputs = testcase + "output_"
         correct = 0
@@ -31,14 +34,36 @@ def bashfunc(filename,testcase,number):
             outi = outputs + str(i+1)
             command1 = "python3 {0} < {1} > tempout.txt".format(filename,inpi)
             command2 = "diff tempout.txt {0} > diff.txt".format(outi)
+            command3 = "rm tempout.txt diff.txt"
             os.system(command1)
             os.system(command2)
             if os.stat("diff.txt").st_size == 0:
                 correct += 1
             else:
+                os.system(command3)
                 return "WA"
+        os.system(command3)
         return "AC"
-    if filename.endswith('.java'):
+    elif lang == 2:
+        inputs = testcase + "input_"
+        outputs = testcase + "output_"
+        correct = 0
+        for i in range(number):
+            inpi = inputs + str(i+1)
+            outi = outputs + str(i+1)
+            command1 = "python {0} < {1} > tempout.txt".format(filename,inpi)
+            command2 = "diff tempout.txt {0} > diff.txt".format(outi)
+            command3 = "rm tempout.txt diff.txt"
+            os.system(command1)
+            os.system(command2)
+            if os.stat("diff.txt").st_size == 0:
+                correct += 1
+            else:
+                os.system(command3)
+                return "WA"
+        os.system(command3)
+        return "AC"
+    elif lang == 3:
         bashCommand = "javac {0}".format(filename)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         if process.communicate()[1]:
@@ -53,10 +78,13 @@ def bashfunc(filename,testcase,number):
                 outi = outputs + str(i+1)
                 command1 = "java {0} < {1} > tempout.txt".format(filei,inpi)
                 command2 = "diff tempout.txt {0} > diff.txt".format(outi)
+                command3 = "rm *.class tempout.txt diff.txt"
                 os.system(command1)
                 os.system(command2)
                 if os.stat("diff.txt").st_size == 0:
                     correct += 1
                 else:
+                    os.system(command3)
                     return "WA"
+            os.system(command3)
             return "AC"
