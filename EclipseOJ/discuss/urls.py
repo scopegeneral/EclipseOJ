0b424c90c9720917e1b7e61bc2c9d99f572452cb
Vamsi Krishna <vamsi3@outlook.com>
2017-10-19 11:21:34 +0530

    Minor Changes in base.css
    - Moved base.html, base.css to homepage.
    (Reasons later.)

--- EclipseOJ/discuss/urls.py
+++ EclipseOJ/discuss/urls.py
@@ -3,5 +3,5 @@ from . import views as discuss_views
 
 urlpatterns = [
     url(r'^$', discuss_views.discuss_index, name='discuss_index'),
-    url(r'(?P<postID>[0-9]+)/$', discuss_views.post_detail, name='discuss_post'),
+    url(r'(?P<postID>[0-9]+)/$', discuss_views.post_detail, name='post_detail'),
 ]
