2e05c840f4165d24d1899004f7f0cfea363c322c
Naman Jain <naman1205jain@gmail.com>
2017-10-24 23:44:58 +0530

    All forms on problems working!!!

--- EclipseOJ/judge/check.py
+++ EclipseOJ/judge/check.py
@@ -1,5 +1,100 @@
 import os
 import subprocess
+from pathlib import Path
+
+def bashoutput(filename,inpi,lang):
+    if lang == 'cpp':
+        compilecommand = "g++ -std=c++14 {0} 2>compile_errors.txt".format(filename)
+        os.system(compilecommand)
+        if os.stat("compile_errors.txt").st_size:
+            contents = Path('compile_errors.txt').read_text()
+            os.system("rm compile_errors.txt")
+            return contents
+        else:
+            command1 = "./test.sh './a.out' < {0} > tempout.txt 2>runtime_errors.txt & PID=$!; sleep {1}; kill $PID 2>time_errors.txt".format(inpi,2)
+            os.system(command1)
+            if os.stat("runtime_errors.txt").st_size:
+                contents = Path('runtime_errors.txt').read_text()
+                os.system("rm a.out {0} {1} tempout.txt compile_errors.txt runtime_errors.txt time_errors.txt".format(inpi,filename))
+                return contents
+            if os.stat("time_errors.txt").st_size == 0:
+                os.system("rm a.out tempout.txt compile_errors.txt runtime_errors.txt time_errors.txt")
+                return "Time Limit Exceeded"
+            contents = Path('tempout.txt').read_text()
+            os.system("rm a.out diff.txt tempout.txt compile_errors.txt runtime_errors.txt time_errors.txt")
+            return contents
+    elif lang == 'py3':
+        command1 = "./test.sh 'python {0}' < {1} > tempout.txt 2>runtime_errors.txt & PID=$!; sleep {2}; kill $PID 2>time_errors.txt".format(filename,inpi,2)
+        os.system(command1)
+        if os.stat("runtime_errors.txt").st_size:
+            contents = Path('runtime_errors.txt').read_text()
+            os.system("rm tempout.txt runtime_errors.txt time_errors.txt")
+            return contents
+        if os.stat("time_errors.txt").st_size == 0:
+            contents = Path('tempout.txt').read_text()
+            os.system("rm tempout.txt runtime_errors.txt time_errors.txt")
+            return "Time Limit Exceeded"
+        contents = Path('tempout.txt').read_text()
+        os.system("rm tempout.txt runtime_errors.txt time_errors.txt")
+        return contents
+    elif lang == 'py':
+        command1 = "./test.sh 'python {0}' < {1} > tempout.txt 2>runtime_errors.txt & PID=$!; sleep {2}; kill $PID 2>time_errors.txt".format(filename,inpi,2)
+        os.system(command1)
+        if os.stat("runtime_errors.txt").st_size:
+            contents = Path('runtime_errors.txt').read_text()
+            os.system("rm tempout.txt runtime_errors.txt time_errors.txt")
+            return contents
+        if os.stat("time_errors.txt").st_size == 0:
+            os.system("rm tempout.txt runtime_errors.txt time_errors.txt")
+            return "Time Limit Exceeded"
+        contents = Path('tempout.txt').read_text()
+        os.system("rm tempout.txt runtime_errors.txt time_errors.txt")
+        return contents
+    if lang == 'c':
+        compilecommand = "gcc -std=c++14 {0} 2>compile_errors.txt".format(filename)
+        os.system(compilecommand)
+        if os.stat("compile_errors.txt").st_size:
+            contents = Path('compile_errors.txt').read_text()
+            os.system("rm compile_errors.txt")
+            return contents
+        else:
+            command1 = "./test.sh './a.out' < {0} > tempout.txt 2>runtime_errors.txt & PID=$!; sleep {1}; kill $PID 2>time_errors.txt".format(inpi,2)
+            os.system(command1)
+            if os.stat("runtime_errors.txt").st_size:
+                contents = Path('runtime_errors.txt').read_text()
+                os.system("rm a.out {0} {1} tempout.txt compile_errors.txt runtime_errors.txt time_errors.txt".format(inpi,filename))
+                return contents
+            if os.stat("time_errors.txt").st_size == 0:
+                os.system("rm a.out tempout.txt compile_errors.txt runtime_errors.txt time_errors.txt")
+                return "Time Limit Exceeded"
+            contents = Path('tempout.txt').read_text()
+            os.system("rm a.out diff.txt tempout.txt compile_errors.txt runtime_errors.txt time_errors.txt")
+            return contents
+    elif lang == 'java':
+        compilecommand = "javac {0} 2>compile_errors.txt".format(filename)
+        os.system(compilecommand)
+        if os.stat("compile_errors.txt").st_size:
+            contents = Path('compile_errors.txt').read_text()
+            os.system("rm compile_errors.txt")
+            return contents
+        filei = filename[:-5]
+        command1 = "./test.sh 'java {0}' < {1} > tempout.txt 2>runtime_errors.txt & PID=$!; sleep {2}; kill $PID 2>time_errors.txt".format(filei,inpi,2)
+        os.system(command1)
+        if os.stat("runtime_errors.txt").st_size:
+            contents = Path('runtime_errors.txt').read_text()
+            os.system("rm *.class tempout.txt runtime_errors.txt time_errors.txt")
+            return contents
+        if os.stat("time_errors.txt").st_size == 0:
+            os.system("rm *.class tempout.txt runtime_errors.txt time_errors.txt")
+            return 'Time Limit Exceeded'
+        os.system("rm tempout.txt runtime_errors.txt time_errors.txt *.class")
+        return ""
+    else:
+        return "no language found"
+
+
+
+
 def bashfunc(filename,testcase,number,lang,timeout):
     if lang == 'cpp':
         compilecommand = "g++ -std=c++14 {0} 2>compile_errors.txt".format(filename)
--- EclipseOJ/problems/forms.py
+++ EclipseOJ/problems/forms.py
@@ -7,5 +7,10 @@ class SubmitForm(forms.ModelForm):
         fields = ['language', 'uploaded_file']
 
 class CodeForm(forms.Form):
-    lang = forms.CharField(max_length=4)
-    code = forms.CharField(widget=forms.Textarea)
+    lang = forms.CharField(max_length=4,widget=forms.HiddenInput)
+    code = forms.CharField(widget=forms.HiddenInput)
+
+class TestForm(forms.Form):
+    test_lang = forms.CharField(max_length=4,widget=forms.HiddenInput)
+    test_code = forms.CharField(widget=forms.HiddenInput)
+    test_input = forms.CharField(max_length=800,widget=forms.HiddenInput,required=False)
