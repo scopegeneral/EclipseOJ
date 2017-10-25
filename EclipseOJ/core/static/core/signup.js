7f40c6e8718574f62a847ec8fb7eae8c7531e9ac
Naman Jain <naman1205jain@gmail.com>
2017-10-25 21:11:01 +0530

    Regex signup

--- EclipseOJ/core/static/core/signup.js
+++ EclipseOJ/core/static/core/signup.js
@@ -1,6 +1,13 @@
 document.getElementById('id_username').onblur = function () {
     var username = $(this).val();
     //alert("Called");
+    var regex = /^[a-zA-Z0-9_@.+-]+$/
+    if (!regex.test(username)){
+      document.getElementsByClassName('help-block')[0].setAttribute("style","color : red");
+      document.getElementsByClassName('help-block')[0].textContent = "Your username should contain only letters, numbers and symbols including _ @ . + -";
+      //document.getElementById('id_username').focus();
+    }
+    else {
     console.log("here!");
     $.ajax({
       url: '/ajax/validate_username/',
@@ -12,7 +19,7 @@ document.getElementById('id_username').onblur = function () {
         if (data.is_taken) {
             document.getElementsByClassName('help-block')[0].setAttribute("style","color : red");
             document.getElementsByClassName('help-block')[0].textContent = "This username has already been taken";
-            document.getElementById('id_username').focus();
+            //document.getElementById('id_username').focus();
         }
         else {
           document.getElementsByClassName('help-block')[0].setAttribute("style","color : green");
@@ -20,4 +27,5 @@ document.getElementById('id_username').onblur = function () {
         }
       }
     });
+    }
 };
