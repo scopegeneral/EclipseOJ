9d94387774e56ca6f7603ab886c137592ed83a61
Naman Jain <naman1205jain@gmail.com>
2017-10-29 01:27:47 +0530

    Modles documentation complete

--- EclipseOJ/core/models.py
+++ EclipseOJ/core/models.py
@@ -10,15 +10,54 @@ def picture_path(instance, filename):
     return 'users/{}/{}'.format(instance.user.username, 'picture')
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to=picture_path, default=static('default_picture.jpg'), blank=True)
-    first_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100, blank=True)
-    email = models.EmailField(max_length=70, blank=True)
-    country = CountryField(default='IN', blank=True)
-    city = models.CharField(max_length=100, blank=True)
-    institute = models.CharField(max_length=100, blank=True)
-    rating = models.IntegerField(default=1500)
+    """
+    This is used to extend default :model:`auth.User` to store extra information about the people using the site.
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        help_text="This is a OneToOneField relationship i.e. User and Profile are uniquly related. Profile extends the default User model. on_delete = CASCADE is true which means if User is deleted then the Profile will be deleted automatically",
+    )
+    picture = models.ImageField(
+        upload_to=picture_path,
+        default=static('default_picture.jpg'),
+        blank=True,
+        help_text="It is an ImageField and is used to store profile picture of user.",
+    )
+    first_name = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text="First name of the user",
+    )
+    last_name = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text="Last name of the user",
+    )
+    email = models.EmailField(
+        max_length=70,
+        blank=True,
+        help_text="This is the email id of the user. The EmailID validator provided by django is used",
+    )
+    country = CountryField(
+        default='IN',
+        blank=True,
+        help_text="This is used to store the country of user. A third party package django-countries has been used which access to all countries. Default has been set to India",
+    )
+    city = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text="This is they city of the user",
+    )
+    institute = models.CharField(
+        max_length=100,
+        blank=True,
+        help_text="This is the institute of the user",
+    )
+    rating = models.IntegerField(
+        default=1500,
+        help_text="Users can compete with each other and rating is the parameter used for competing with each other. Rating are updated after each contest through probabilistic analysis of the contest result",
+    )
 
     def __str__(self):
         return self.user.username
